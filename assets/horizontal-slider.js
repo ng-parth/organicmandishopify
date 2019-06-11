0a065896095fc4ce77a330f6e272b153b69f9bdc
Parth <ppmistry204@gmail.com>
2019-06-11 19:58:52 +0530

    Initial Commit

---
+++ assets/horizontal-slider.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+ $('#farmer-carousel').owlnewCarousel({
+      stagePadding: 40,
+      loop: true,
+      margin: 15,
+      nav: true,
+      dots: false,
+   	lazyload: true,
+  
+      responsive:{
+        0:{
+          items:1
+        },
+        800:{
+          items:2
+        },
+        1000:{
+          items:3
+        }
+      }
+    });
+
+});
+
+
+
